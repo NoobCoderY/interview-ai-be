79c91377fe9cc91b3559f89cb03cc84f2e47815b
YashBaryTech <btidiwakerya@barytech.com>
2026-04-19 19:04:18 +0530

    add cors  origin

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ import paymentRouter from "./routes/payment.route.js"
 
 const app = express()
 app.use(cors({
-    origin:["http://localhost:5173","http://localhost:5174","https://interview-ai-fe-ten.vercel.app/"],
+    origin:["http://localhost:5173","http://localhost:5174","https://interview-ai-fe-ten.vercel.app"],
     credentials:true
 }))
 
